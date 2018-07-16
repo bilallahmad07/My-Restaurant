ed29ef68fd36c9b86c24c0afb02584095cf10789
Bilal Ahmad <bilallahmad07@gmail.com>
2018-07-16 12:49:43 +0530

    update

--- app/restaurantApp.js
+++ app/restaurantApp.js
@@ -1,10 +1,44 @@
 var restaurantApp = angular.module("restaurantApp", []);
 restaurantApp.controller("restaurantCotroller",restaurantFunc);
 function restaurantFunc($scope,$http){
+	$scope.catgry = [];
+	$scope.chinesecombo = [];
+	$scope.chinesestarter = [];
+	$scope.salad = [];
+	$scope.oriental = [];
 
 	$http.get("https://thesmartq.firebaseio.com/menu.json")
     .then(function(response) {
-        $scope.restaurantMenu = response.data;
+		$scope.restaurantMenu = response.data;
+		//console.log($scope.restaurantMenu.length);
+		//console.log(response);
+		//console.log(response.data.length);
+		var m = 0;
+		for (var i=0; i<response.data.length; i++){
+			//console.log(response.data[i].category);
+			if($scope.catgry.indexOf(response.data[i].category) == -1){
+				$scope.catgry.push(response.data[i].category);
+				//$scope.catgryitems[m++][0] = response.data[i].category;
+				//$scope.catgryitems.push(response.data[i].category);
+			}				
+		}
+		console.log($scope.catgryitems);
+		for (var i=0; i<response.data.length; i++){
+			//var tmp = $scope.catgry.indexOf();
+			if(response.data[i].category === 'Chinese Combos'){
+				$scope.chinesecombo.push(response.data[i]);
+			}
+			else if(response.data[i].category === 'Chinese Starter'){
+				$scope.chinesestarter.push(response.data[i]);
+			}
+			else if(response.data[i].category === 'Salads'){
+				$scope.salad.push(response.data[i]);
+			}
+			else{
+				$scope.oriental.push(response.data[i]);
+			}
+		}
+		
     });
 	
 	
